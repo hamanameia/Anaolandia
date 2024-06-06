448f332afdcca825a7fc95e9dbd8a31afa88f70b
Hamanameia <hamanameia1@gmail.com>
2024-06-06 15:53:28 -0300

    Senai Hoje

--- Anaolandia/Paginas/PaginaUsuario.xaml.cs
+++ Anaolandia/Paginas/PaginaUsuario.xaml.cs
@@ -6,4 +6,20 @@ public partial class PaginaUsuario : ContentPage
 	{
 		InitializeComponent();
 	}
+    private async void Home_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Home());
+    }
+    private async void Sucesso_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Sucesso());
+    }
+    private async void Lojinha_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Lojinha());
+    }
+    private async void Usuario_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new PaginaUsuario());
+    }
 }
